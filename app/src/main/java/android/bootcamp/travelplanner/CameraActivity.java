d1683729e41674f5d5d2a381dbcd6d80d9c8d9fe
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-03-16 16:50:28 +0530

    Added a camera handling activity in the project, added corresponding intent filter.

---
+++ app/src/main/java/android/bootcamp/travelplanner/CameraActivity.java
@@ -0,0 +1,13 @@
+package android.bootcamp.travelplanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CameraActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
