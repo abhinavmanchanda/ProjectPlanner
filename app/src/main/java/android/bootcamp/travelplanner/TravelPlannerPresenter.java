f0d5767fbea569ab29cf2d61148a7cc632e066b4
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2018-06-18 16:03:03 +0530

    Made time and time buffer observable so that dynamic changes in their values are reflected on the UI.

--- app/src/main/java/android/bootcamp/travelplanner/TravelPlannerPresenter.java
+++ app/src/main/java/android/bootcamp/travelplanner/TravelPlannerPresenter.java
@@ -10,7 +10,8 @@ public class TravelPlannerPresenter {
     private TravelPlannerRepository repository;
     public final ObservableField<String> distance = new ObservableField<>();
     public final ObservableField<String> velocity = new ObservableField<>();
-    public String time;
+    public final ObservableField<String> time = new ObservableField<>();
+    public final ObservableField<String> timeWithBuffer = new ObservableField<>();
 
     public TravelPlannerPresenter(TravelPlannerView view, TravelPlannerRepository repository) {
         this.view = view;
@@ -18,9 +19,9 @@ public class TravelPlannerPresenter {
     }
 
     public void calculate(){
-        time = String.valueOf(parseInt(distance.get()) / parseInt(velocity.get()));
+        time.set(String.valueOf(parseInt(distance.get()) / parseInt(velocity.get())));
 
-        view.launchTimeActivityWithTimeParameter(time);
+        view.launchTimeActivityWithTimeParameter(time.get());
     }
 
     public void capture() {
@@ -29,7 +30,7 @@ public class TravelPlannerPresenter {
 
 
     public void processBufferReturned(String buffer) {
-        view.displayBuffer(buffer);
+        timeWithBuffer.set(buffer);
     }
 
     public void save(String distance, String velocity, String time, String timeWithBuffer) {
